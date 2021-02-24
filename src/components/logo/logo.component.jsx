c8f38c556b739a88439604788c5c5125e0c01da2
oladele.olorunsola <oladele.olorunsola@bonial.com>
2021-02-24 14:38:30 +0100

    design frontpage with vector art

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -1,6 +1,6 @@
 import logoImg from '../../assets/logo.png';
 const Logo = () => (
-   <div>
+   <div className="py-8">
       <img src={logoImg} alt="digivote logo" width="192px"/>
    </div>
 );
--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -2,13 +2,13 @@ import Header from "../../components/header/header.component";
 import "./home.css";
 
 const Home = () => (
-      <div className = "container mx-auto px-2 h-screen" >
-         <div className= "py-10">
+      <div className = "container mx-auto" >
+         <div>
             <Header />
-            <div className="flex py-20">
+            <div className="flex py-20 content-center context md:flex-1">
                <div className="flex-grow-0 w-5/12">
                   <h2 className="textColor text-7xl leading-tight font-medium">Explore <span className="brandColor">New Ways</span> to <span className="brandColor">VOTE</span></h2>
-                  <p className="py-3 subtext">A solo project that combines the idea of the blockchain technology to improve the fundamental right of any individual (The rights to VOTE).</p>
+                  <p className="py-3 subtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed porta turpis. Sed rhoncus massa feugiat ultricies ultrices. Suspendisse egestas augue quis felis varius.</p>
                   <div className="flex space-x-5 py-4">
                      <div>
                         <button className="rounded-full py-3 px-6 btn">Create Election</button>
@@ -18,9 +18,7 @@ const Home = () => (
                      </div>
                   </div>
                </div>
-               <div className="flex-grow-0 w-2/3">
-                  
-               </div>
+               <div className="flex-grow-0 w-2/3 electionBg"></div>
             </div>
          </div>
       </div>
