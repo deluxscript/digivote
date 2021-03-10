6e4b87d81490ca7fe689b408b0acc55afa082e2f
oladele.olorunsola <oladele.olorunsola@bonial.com>
2021-03-10 10:40:59 +0100

    Register page functionalities

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 import './App.css';
+import Header from "./components/header/header.component";
 import Layout from './hoc/Layout/Layout.component';
 import Home from './pages/home/home.component';
+import Register from "./pages/Register/Register.component";
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Home />
-      </Layout>
-    </div>
+   <Router>
+      <div className="App container mx-auto">
+         <Layout>
+            <Header />
+            <Switch>
+               <Route path="/register">
+                  <Register />
+               </Route>
+               <Route path="/">
+                  <Home />
+               </Route>
+            </Switch>
+         </Layout>
+      </div>
+   </Router>
   );
 }
 
